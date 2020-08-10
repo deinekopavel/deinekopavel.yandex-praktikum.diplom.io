ad28b4a6a7139b89e3be037857239fc1bb7821b2
Deineko Pavel <deinekopavel@yandex.ru>
2020-08-10 22:23:19 +0300

    build for rev

--- src/js/about.js
+++ src/js/about.js
@@ -1,2 +1,51 @@
 import "../pages/about.css";
-console.log ("about");
+import "./swiper";
+
+const swipe = document.querySelector('.swipe');
+let mySwiper = new Swiper (swipe, {
+    /*centeredSlides: true,*/
+    grabCursor: true,
+    /*freemode: true,*/
+    slideClass: 'slide',
+    wrapperClass: 'swipe__wrapper',
+    breakpoints: {
+        320: {
+            slidesPerView: 1.1,
+            spaceBetween: 8,
+            loop: false,
+        },
+
+        400: {
+            slidesPerView: 1.5,
+            spaceBetween: 8,
+            loop: false,
+        },
+
+        660: {
+            slidesPerView: 2,
+            spaceBetween: 8,
+            loop: false,
+        },
+        768: {
+            slidesPerView: 2.2558,
+            spaceBetween: 8,
+            loop: false,
+        },
+
+        1024: {
+            slidesPerView: 3.5,
+            spaceBetween: 16,
+            centeredSlides: true,
+            loop: true,
+        }
+
+    },
+    navigation: {
+        nextEl: '.swipe__button_next',
+        prevEl: '.swipe__button_prev',
+    },
+    pagination: {
+        el: '.swipe__pagination',
+        type: 'bullets',
+    },
+})
--- src/js/paper.js
+++ src/js/paper.js
@@ -1,2 +1,1 @@
 import "../pages/paper.css";
-console.log ("paper");
